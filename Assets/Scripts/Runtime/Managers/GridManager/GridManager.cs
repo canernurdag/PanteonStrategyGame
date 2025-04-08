16d91ad8ba4762ed407b101342c2ebabf2851324
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2025-04-08 16:39:32 +0300

    Implement: GridCenterPosition Calculation to GridManager

--- Assets/Scripts/Runtime/Managers/GridManager/GridManager.cs
+++ Assets/Scripts/Runtime/Managers/GridManager/GridManager.cs
@@ -1,42 +1,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : Singleton<GridManager>
+public sealed class GridManager : Singleton<GridManager>
 {
 	#region DIRECT REF
 	[SerializeField] private NodeFactory _nodeFactory;
 	#endregion
+
+	#region INTERNAL VARIABLES
+	private OnGridCenterPositionDetermined _onGridCenterPositionDetermined;
+
+
 	public int width = 20;
 	public int height = 20;
 	public float cellSize = 1f;
 
-	private Node[,] grid;
+	private Node[,] _grid;
+	private Vector3 _gridCenterPosition;
+
+	#endregion
 
 	protected override void Awake()
 	{
 		base.Awake();
 		CreateGrid();
+
+		
 	}
 
-	void CreateGrid()
+	private void Start()
 	{
-		grid = new Node[width, height];
+		_onGridCenterPositionDetermined = EventManager.Instance.GetEvent<OnGridCenterPositionDetermined>();
+
+		_gridCenterPosition = GetGridCenterPosition();
+		_onGridCenterPositionDetermined.Execute(_gridCenterPosition);
+	}
+
+
+
+	private void CreateGrid()
+	{
+		_grid = new Node[width, height];
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < height; y++)
 			{
 				Vector3 worldPos = new Vector3(x, y) * cellSize;
-				grid[x, y] = _nodeFactory?.CreateNode();
-				grid[x, y].Init(x, y, worldPos, transform);
+				_grid[x, y] = _nodeFactory?.CreateNode();
+				_grid[x, y].Init(x, y, worldPos, transform);
 			}
 		}
+	}
+
+	private Vector3 GetGridCenterPosition()
+	{
+		Vector3 centerPos = Vector3.zero;
+		int width = _grid.GetLength(0);
+		int height = _grid.GetLength(1);
+
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				var node = _grid[i, j];
+				centerPos += node.transform.position;
+			}
+		}
+
+		int totalNodeCount = width * height;
+		return centerPos / (float)totalNodeCount;
 	}
 
 	public Node GetNodeFromWorldPosition(Vector3 worldPosition)
 	{
 		int x = Mathf.RoundToInt(worldPosition.x / cellSize);
 		int y = Mathf.RoundToInt(worldPosition.y / cellSize);
-		if (IsInBounds(x, y)) return grid[x, y];
+		if (IsInBounds(x, y)) return _grid[x, y];
 		return null;
 	}
 
@@ -51,7 +90,7 @@ public class GridManager : Singleton<GridManager>
 				int checkX = node.X + dx;
 				int checkY = node.Y + dy;
 				if (IsInBounds(checkX, checkY))
-					neighbours.Add(grid[checkX, checkY]);
+					neighbours.Add(_grid[checkX, checkY]);
 			}
 		}
 		return neighbours;
